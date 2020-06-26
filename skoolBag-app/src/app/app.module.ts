d9e20006979ed44faac54f8e1f08945e56e18f81
sara-91 <dsnmahagama@gmail.com>
2020-06-26 10:34:51 +0530

    Basic UI changes added with tabset

--- skoolBag-app/src/app/app.module.ts
+++ skoolBag-app/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+
+import { AppComponent } from "./app.component";
+import { SchoolListComponent } from "./components/school-list/school-list.component";
+import { AddSchoolComponent } from "./components/add-school/add-school.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, SchoolListComponent, AddSchoolComponent],
+  imports: [BrowserModule, NgbModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
