496dc88c52fe4b84dae39deb60c0f51b890493f1
sara-91 <dsnmahagama@gmail.com>
2020-06-26 19:48:23 +0530

    Firebase installed, view-list initial done

--- skoolBag-app/src/app/app.module.ts
+++ skoolBag-app/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { NgModule } from "@angular/core";
 
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireDatabaseModule } from "@angular/fire/database";
+import { environment } from "../environments/environment";
+
 import { AppComponent } from "./app.component";
 import { SchoolListComponent } from "./components/school-list/school-list.component";
 import { AddSchoolComponent } from "./components/add-school/add-school.component";
+import { SchoolService } from "./services/school.service";
 
 @NgModule({
   declarations: [AppComponent, SchoolListComponent, AddSchoolComponent],
-  imports: [BrowserModule, NgbModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireDatabaseModule,
+  ],
+  providers: [SchoolService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
