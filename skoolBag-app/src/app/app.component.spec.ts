c2efd4d7b74cb23cb161c1fbc9435be3b948171a
sara-91 <dsnmahagama@gmail.com>
2020-06-29 05:32:57 +0530

    unit tests setup and few unit tests added

--- skoolBag-app/src/app/app.component.spec.ts
+++ skoolBag-app/src/app/app.component.spec.ts
@@ -1,16 +1,30 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { SchoolService } from "./services/school.service";
+import { BrowserModule } from "@angular/platform-browser";
+import { environment } from "src/environments/environment";
+import { AngularFireModule } from "@angular/fire";
+import { FormBuilder, FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AddSchoolComponent } from "src/app/components/add-school/add-school.component";
+import { SchoolListComponent } from "src/app/components/school-list/school-list.component";
 
-describe('AppComponent', () => {
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
+      declarations: [AddSchoolComponent, SchoolListComponent, AppComponent],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
       ],
+      providers: [FormBuilder, SchoolService],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it("should create the app", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
@@ -19,13 +33,15 @@ describe('AppComponent', () => {
   it(`should have as title 'skoolBag-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('skoolBag-app');
+    expect(app.title).toEqual("skoolBag-app");
   });
 
-  it('should render title', () => {
+  it("should render title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('skoolBag-app app is running!');
+    expect(compiled.querySelector(".container span h1").textContent).toContain(
+      "School Details"
+    );
   });
 });
--- skoolBag-app/src/app/components/school-list/school-list.component.spec.ts
+++ skoolBag-app/src/app/components/school-list/school-list.component.spec.ts
@@ -1,16 +1,29 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { AngularFireModule } from "@angular/fire";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { SchoolListComponent } from './school-list.component';
+import { SchoolListComponent } from "./school-list.component";
+import { School } from "src/app/models/school";
+import { Address } from "src/app/models/address";
+import { SchoolService } from "src/app/services/school.service";
+import { AppComponent } from "src/app/app.component";
+import { environment } from "src/environments/environment";
 
-describe('SchoolListComponent', () => {
+describe("SchoolListComponent", () => {
   let component: SchoolListComponent;
   let fixture: ComponentFixture<SchoolListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SchoolListComponent ]
-    })
-    .compileComponents();
+      imports: [
+        BrowserModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+      ],
+      declarations: [SchoolListComponent, AppComponent],
+      providers: [SchoolService],
+      schemas: [NO_ERRORS_SCHEMA],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +32,7 @@ describe('SchoolListComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
