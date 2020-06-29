17f4d49da12890390ac00343d8eac7f7f17f77f3
sara-91 <dsnmahagama@gmail.com>
2020-06-29 08:47:43 +0530

    firebase deployment done

--- skoolBag-app/src/environments/environment.prod.ts
+++ skoolBag-app/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC9awQ-HugPg_wOWIvOBtI_jMAq8BRdRAo",
+    authDomain: "skoolbagapp-9c953.firebaseapp.com",
+    databaseURL: "https://skoolbagapp-9c953.firebaseio.com",
+    projectId: "skoolbagapp-9c953",
+    storageBucket: "skoolbagapp-9c953.appspot.com",
+    messagingSenderId: "770221569677",
+    appId: "1:770221569677:web:9d7033ebc4ba61d849d4c4",
+  },
 };
